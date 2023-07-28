eb8564b1f00db2e92f8d8af8c50ca4f88d34ab5f
evanXXI <evan.rktcisse@gmail.com>
2023-07-28 11:57:40 +0200

    :construction: :sparkles: :lipstick: home responsive, style and dynamism
    Fixes for the slider that was'nt sliding on click, and temporary fixes for the responsive design of category icons, navbar and style

--- homepage/category.js
+++ homepage/category.js
@@ -1,3 +1,20 @@
+$("header nav div #cartNavbarToggleDisplay").hide();
+
+const cartBtn = $("<button></button>").addClass("btn");
+cartBtn.attr("id", "cartBtn");
+cartBtn.html("<i class='bi bi-cart'></i>");
+
+$("header nav div .navbar-toggler").click(() => {
+    $("header nav div #cartBtn").hide();
+    $("header nav div #cartNavbarToggleDisplay").show();
+    
+    /* $("header nav div .navbar-toggler").click(() => {
+        $("header nav div #cartBtn").show();
+        $("header nav div #cartNavbarToggleDisplay").hide();
+    }); */
+});
+
+
 $.ajax({
     url: "../php/category.php",
     type: "GET",
@@ -10,18 +27,20 @@ $.ajax({
             console.log(res.categories);
 
             res.categories.forEach(cat => {
-                const cardCtn = $("<div></div>").addClass("card bg-dark text-white m-3");
+                const cardCtn = $("<div></div>").addClass("card-group");
+                const card = $("<div></div>").addClass("card text-white m-3");
 
-                const img = $("<img>").addClass("card-img p-5");
+                const img = $("<img>").addClass("card-img h-100 w-100 p-4");
                 img.attr("src", "../assets/" + cat.image);
                 img.attr("alt", cat.image);
 
-                const cardTitle = $("<h2></h2>").addClass("card-title fs-4").text(cat.name);
+                const cardTitle = $("<h2></h2>").addClass("card-title fs-6 w-100").text(cat.name);
                 
                 const cardOverlay = $("<div></div>").addClass("card-img-overlay");
                 
                 cardOverlay.append(cardTitle);
-                cardCtn.append(img, cardOverlay);
+                card.append(img, cardOverlay);
+                cardCtn.append(card);
                 $("#categoryCtn").append(cardCtn);
             });
         }
