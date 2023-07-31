63bc86a1a282de2b10abe2541a0b1c598f5c6d15
evanXXI <evan.rktcisse@gmail.com>
2023-07-31 23:43:57 +0200

    :sparkles: :lipstick: added footer, dynamic cart button and and styles

--- homepage/category.js
+++ homepage/category.js
@@ -1,17 +1,8 @@
-$("header nav div #cartNavbarToggleDisplay").hide();
-
-const cartBtn = $("<button></button>").addClass("btn");
-cartBtn.attr("id", "cartBtn");
-cartBtn.html("<i class='bi bi-cart'></i>");
+$("header nav div #cartBtnBurger").hide();
 
 $("header nav div .navbar-toggler").click(() => {
-    $("header nav div #cartBtn").hide();
-    $("header nav div #cartNavbarToggleDisplay").show();
-    
-    /* $("header nav div .navbar-toggler").click(() => {
-        $("header nav div #cartBtn").show();
-        $("header nav div #cartNavbarToggleDisplay").hide();
-    }); */
+    $("header nav div #cartBtnBasic").hide();
+    $("header nav div #cartBtnBurger").show();
 });
 
 
@@ -28,18 +19,21 @@ $.ajax({
 
             res.categories.forEach(cat => {
                 const cardCtn = $("<div></div>").addClass("card-group");
+                
                 const card = $("<div></div>").addClass("card text-white m-3");
-
+                
                 const img = $("<img>").addClass("card-img h-100 w-100 p-4");
                 img.attr("src", "../assets/" + cat.image);
                 img.attr("alt", cat.image);
 
                 const cardTitle = $("<h2></h2>").addClass("card-title fs-6 w-100").text(cat.name);
-                
+
+                const cardLink = $("<a></a>").addClass("stretched-link");
+                cardLink.attr("href", "homepage");
                 const cardOverlay = $("<div></div>").addClass("card-img-overlay");
                 
                 cardOverlay.append(cardTitle);
-                card.append(img, cardOverlay);
+                card.append(img, cardLink, cardOverlay);
                 cardCtn.append(card);
                 $("#categoryCtn").append(cardCtn);
             });
