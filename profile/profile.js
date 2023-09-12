55d42587fe2b17190247857ea10f1044a1e9e0e6
Evan <evan.rktcisse@gmail.com>
2023-09-12 15:23:19 +0200

    :sparkles: fixed user infos update function

--- profile/profile.js
+++ profile/profile.js
@@ -12,7 +12,6 @@ $.ajax({
             $("#lastname").val(res.user.lastname);
             $("#firstname").val(res.user.firstname);
             $("#birthdate").val(res.user.birthdate);
-            
             $("#street_no").val(res.user.street_number);
             $("#street_extension").val(res.user.street_bis);
             $("#street_name").val(res.user.street_name);
@@ -20,53 +19,98 @@ $.ajax({
             $("#country").val(res.user.country);
             $("#email").val(res.user.email);
         }else{
-            console.log(res.error);
+            alert(res.error);
         }
     }
 });
 
 //* Modification des infos personnelles
-$("#modify").click(() => {
-    
+$("#modify").click(e => {
+    e.preventDefault();
+
     $.ajax({
         url: "../php/users.php",
         type: "POST",
         dataType: "json",
         data: {
             choice: "update",
-            id: user.id
+            id: user.id,
+            firstname: $("#firstname").val(),
+            lastname: $("#lastname").val(),
+            birthdate: $("#birthdate").val(),
+            street_number: $("#street_no").val(),
+            street_bis: $("#street_extension").val(),
+            street_name: $("#street_name").val(),
+            zip_code: $("#zip_code").val(),
+            country: $("#country").val(),
+            email: $("#email").val()
         },
         success: (res) => {
             if (res.success) {
-                console.log(res);
-            }else console.log(res.error);
+                alert("Modification réussie !");
+            }else alert(res.error);
         }
     });
+});
+
+//** Affichage des commandes de l'utilisateur **//
+$.ajax({
+    url: "../php/orders.php",
+    type: "GET",
+    dataType: "json",
+    data: {
+        choice: "select",
+        user_id: user.id
+    },
+    success: (res) => {
+        if (res.success) {
+            res.myOrders.forEach(order => {
+                const row = $("<tr></tr>");
+    
+                const order_no = $("<td></td>").text(order.order_id);
+    
+                const order_tm = $("<td></td>").text(order.order_time);
+    
+                const price = $("<td></td>").text(order.total_price);
+                price.addClass("d-none d-lg-table-cell");
+    
+                const statusCtn = $("<td></td>").text(order.order_status);
+    
+                const address = $("<td></td>").text(order.user_address);
+                address.addClass("d-none d-lg-table-cell");
+    
+                row.append(order_no, order_tm, price, statusCtn, user, address);
+    
+                $("#ordersCtn tbody").append(row);
+                });
+        }else alert(res.error);
+    }
 });
 
 //** NAVIGATION **//
 $("div div div ul li a[href='profile.html?favorites']").click(e => {
     e.preventDefault();
     $("#userForm").addClass("d-none");
-    console.log("Ok click !!!")
 });
 
 $("div div div ul li a[href='profile.html?orders']").click(e => {
     e.preventDefault();
     $("#userForm").addClass("d-none");
-    console.log("Ok click !!!")
+    $("#ordersCtn").removeClass("d-none");
+
+    $("#ordersCtn").removeClass("d-block");
 });
 
 $("div div div ul li a[href='profile.html?messages']").click(e => {
     e.preventDefault();
     $("#userForm").addClass("d-none");
-    console.log("Ok click !!!");
 });
 
 $("div div div ul li a[href='profile.html?infos']").click(e => {
     e.preventDefault();
+    $("#ordersCtn").addClass("d-none");
+
     $("#userForm").removeClass("d-none");
     $("#userForm").addClass("d-block");
-    console.log("Ok click !!!");
 });
 
